44fd02bd5c2ca82e249826edd9f87475df83dbe3
linhlq_58 <lequyenlinh110@gmail.com>
2023-05-07 20:57:04 +0700

    Fix JWT bug

--- starter_code/src/main/java/com/example/demo/security/JWTAuthenticationVerificationFilter.java
+++ starter_code/src/main/java/com/example/demo/security/JWTAuthenticationVerificationFilter.java
@@ -5,6 +5,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -16,6 +17,7 @@ import java.util.ArrayList;
 
 import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
 
+@Component
 public class JWTAuthenticationVerificationFilter extends BasicAuthenticationFilter {
     public JWTAuthenticationVerificationFilter(AuthenticationManager authenticationManager) {
         super(authenticationManager);
